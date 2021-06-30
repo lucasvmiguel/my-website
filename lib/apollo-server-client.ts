5e3099f48dfea4a43d96a4d05cea8340609663a1
Lucas Vieira Miguel <lucasvieira.dev@gmail.com>
2021-07-01 00:19:17 +0100

    Revert "added auth with auth0"

--- lib/apollo-server-client.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: process.env.HASURA_APOLLO_CLIENT_URI,
-  cache: new InMemoryCache(),
-  headers: {
-    'X-Hasura-Admin-Secret': process.env.HASURA_APOLLO_CLIENT_TOKEN as string
-  }
-})
