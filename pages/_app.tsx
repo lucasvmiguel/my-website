7b2bbf6d5a4dd136ebe97654940133aa17775972
Lucas Miguel <lucasvieira.dev@gmail.com>
2021-07-02 15:41:22 +0100

    added apollo with hasura access

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,35 @@
+import { PropsWithChildren } from 'react'
 import type { AppProps } from 'next/app'
+
+import useSWR from 'swr'
 import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from '@auth0/nextjs-auth0'
+import { ApolloProvider } from '@apollo/client'
+
+import { createClient } from '../lib/apollo'
+import fetcher from '../lib/fetcher'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <ApolloProviderComponent>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProviderComponent>
     </UserProvider>
   )
 }
+
+const ApolloProviderComponent = ({ children }: { children: PropsWithChildren<{}> }) => {
+  const { data } = useSWR('/api/get-token', fetcher)
+  const client = createClient(data?.token)
+
+  return (
+    <ApolloProvider client={client}>
+      {children}
+    </ApolloProvider>
+  )
+}
+
 export default MyApp
