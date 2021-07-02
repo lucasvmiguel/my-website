eca37444cdbf5357111f76382cff35b81b98be2f
Lucas Miguel <lucasvieira.dev@gmail.com>
2021-07-02 18:04:50 +0100

    proxying graphql requests from next.js API to hasura server, avoiding exposing the user token in the front

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,20 @@
-import { PropsWithChildren } from 'react'
 import type { AppProps } from 'next/app'
 
-import useSWR from 'swr'
 import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from '@auth0/nextjs-auth0'
 import { ApolloProvider } from '@apollo/client'
 
-import { createClient } from '../lib/apollo'
-import fetcher from '../lib/fetcher'
+import { client } from '../lib/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <ApolloProviderComponent>
+      <ApolloProvider client={client}>
         <ChakraProvider>
           <Component {...pageProps} />
         </ChakraProvider>
-      </ApolloProviderComponent>
+      </ApolloProvider>
     </UserProvider>
-  )
-}
-
-const ApolloProviderComponent = ({ children }: { children: PropsWithChildren<{}> }) => {
-  const { data } = useSWR('/api/get-token', fetcher)
-  const client = createClient(data?.token)
-
-  return (
-    <ApolloProvider client={client}>
-      {children}
-    </ApolloProvider>
   )
 }
 
